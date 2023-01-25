b15a845e778f3b0adea13fb0f801cc060139a7a9
anoopraj467 <60770471+anoopraj467@users.noreply.github.com>
2023-01-25 12:43:21 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,13 +1,12 @@
+provider "azurerm" {
+  features {}
+}
+
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
-      version = "3.37.0"
+      source  = "hashicorp/azurerm"
+      version = "2.39.0"
     }
-  }
-}
-
-provider "azurerm" {
-  features {
   }
 }
